9a5c2f9c6a3b0295f0ddc34a23f91c2209997294
vasilev.andrei@phystech.edu <vasilev.andrei@phystech.edu>
2021-10-10 15:55:41 +0700

    Рефакторинг 3-ей лабы

--- inf_1_sem/lab5/02.py
+++ inf_1_sem/lab5/02.py
@@ -1,49 +1,37 @@
+# -*- coding: utf-8 -*-
 import pygame as pg
-from pygame.draw import * 
 import numpy as np
- 
-pg.init() 
- 
-FPS = 30 
-screen = pg.display.set_mode((927+500-100, 769)) 
-f1 = pg.Surface((927, 769))
-f2 = pg.Surface((927, 769))
-myfont = pg.font.SysFont('arial', 60)
-textsurface = myfont.render('Python realllllly sucks!!!', False, (0, 0, 0))
-
-
-
-bl = (0,0,0)
-wh = (255,255,255,255)
-
-f1.fill(wh)
-f2.fill(wh)
-screen.fill(wh)
-
-
-ye = (255,212,42,255)
-gr =(0,128,0,255)
-ar =(255,102,0,255)
-re = (255,42,42,255)
-pu = (212,42,255,255)
-br = (120,68,33,255)
-go = (128,179,255,255)
-se = (190,200,183,255)
-sc = (233,198,175,255)
-
-lg = (127,255,42,255)
-
 
 def trig(f,x,y,l,a,cl):
+	 #Вспомогательная функция, вызывается в функции рисования человечка
+	 #Рисует волос человечка
+	 #f - объект pygame.Surface
+	 #x, y - координаты одной из вершин волоса изображения
+	 #cl - цвет
+	 #bl - чёрный цвет
+	 #l - размеры волоса
+	 #a - угол поворота
      pg.draw.polygon(f,cl,([x,y],[int(x+l*np.cos(a)),int(y-l*np.sin(a))],[int(x+l*np.cos(a+1.047)),int(y - np.sin(a+1.047)*l)]))
      pg.draw.polygon(f,bl,([x,y],[int(x+l*np.cos(a)),int(y-l*np.sin(a))],[int(x+l*np.cos(a+1.047)),int(y - np.sin(a+1.047)*l)]),1)
-
-
-def man(s,ck,cg,cm,cv):
-
-
-
-    #body
+	 
+def man(ck,cg,cm,cv,cn,cr,x,y,a,d):
+	#Основная функция
+	#Рисует человечка
+	#ck - Цвет рук, кистей и лица
+	#cg - Цвет глаз
+	#cm - Цвет рубашки
+	#cv - Цвет волос
+	#cn - Цвет носа
+	#cr - Цвет рта
+	#x,y - координаты левого верхнего угла человечка
+	#a,d - увеличение размера по вертикали и горизонтали
+	
+	
+    #s - объект pygame.Surface
+    s = pg.Surface((927, 769), pg.SRCALPHA) 
+	
+	
+	#Тело
     pg.draw.circle(s,cm,(447,769),246,0)
 
     pg.draw.polygon(s,ck,([669,558],[700,573],[854,124],[823,124]),0)
@@ -54,14 +42,14 @@ def man(s,ck,cg,cm,cv):
     pg.draw.ellipse(s,wh,(42,40,85,95),1)
     pg.draw.ellipse(s,wh,(790,36,87,110),1)
 
-
     pg.draw.polygon(s,cm,([174,578],[258,531],[316,600],[272,690],[185,676]),0)
     pg.draw.polygon(s,bl,([174,578],[258,531],[316,600],[272,690],[185,676]),1)
 
     pg.draw.polygon(s,cm,([585,607],[642,531],[724,568],[720,668],[636,692]),0)
     pg.draw.polygon(s,bl,([585,607],[642,531],[724,568],[720,668],[636,692]),1)
 
-    #head
+	
+    #Голова
     pg.draw.ellipse(s,ck,(222,168,455,463),0)
     pg.draw.ellipse(s,cg,(327,316,99,87),0)
     pg.draw.ellipse(s,bl,(327,316,99,87),1)
@@ -72,12 +60,13 @@ def man(s,ck,cg,cm,cv):
     pg.draw.ellipse(s,bl,(509,350,28,28),0)
 
 
-    pg.draw.polygon(s,br,([433,416],[471,416],[452,450]),0)
-    pg.draw.polygon(s,re,([324,467],[568,467],[454,545]),0)
+    pg.draw.polygon(s,cn,([433,416],[471,416],[452,450]),0)
+    pg.draw.polygon(s,cr,([324,467],[568,467],[454,545]),0)
     pg.draw.polygon(s,bl,([433,416],[471,416],[452,450]),1)
     pg.draw.polygon(s,bl,([324,467],[568,467],[454,545]),1)
 
-    #volos
+	
+    #Волосы
     L = 72
 
     trig(s,252,286,L,1.01,cv)
@@ -90,26 +79,60 @@ def man(s,ck,cg,cm,cv):
     trig(s,538,183,L,-0.57,cv)
     trig(s,576,201,L,-0.74,cv)
     trig(s,611,230,L,-1,cv)
+	
+	
+    s = pg.transform.smoothscale(s,(int(a), int(d)))
+    screen.blit(s, (x,y))
+	
+def tablichka(x,y,a,d,sign):
+	#Основная функция
+	#Рисует табличку с напписью
+	#x,y - координаты левого верхнего угла человечка
+	#a,d - размеры по вертикали и горизонтали соответственно
+	#sign - надпись, строкового типа
+	
+	#s - объект pygame.Surface
+	s = pg.Surface((927, 90), pg.SRCALPHA)
+	
+	myfont = pg.font.SysFont('arial', 60)
+	textsurface = myfont.render(sign, False, (0, 0, 0))
+	
+	pg.draw.rect(s,lg,(0,0,927,90),0)	# Рисует надпись (про питон что-то нецензурное)
+	pg.draw.rect(s,bl,(0,0,927,90),1)
+	u = myfont.size(sign)
+	s.blit(textsurface,(int((927 - u[0])/2),0))
+	s = pg.transform.smoothscale(s,(int(a), int(d)))
+	screen.blit(s, (x,y))
+	
+pg.init() 
+ 
+#НАЧАЛО РИСУНКА 
+ 
+FPS = 30 
+screen = pg.display.set_mode((1500, 769)) 
 
-    return s
+
+#Библиотека цветов
+bl = (0,0,0,255)		#чёрный
+wh = (255,255,255,255)  #белый
+ye = (255,212,42,255)	#жёлтый
+gr =(0,128,0,255)		#зелёный
+ar =(255,102,0,255)		#оранжевый
+re = (255,42,42,255)	#красный
+pu = (212,42,255,255)	#фиолетовый
+br = (120,68,33,255)	#коричневый
+go = (128,179,255,255)	#голубой
+se = (190,200,183,255)	#серый
+sc = (233,198,175,255)	#бежевый
+lg = (127,255,42,255)	#токсично-зелёный
 
 
-f1 = man(f1, sc,go,ar,pu)
+screen.fill(wh)
+man(sc,go,ar,pu,br,re,0,0,750,769)
+man(br,gr,pu,lg,sc,re,750,0,750,769)
+tablichka(0,0,1500,90,'Python sucks!!!')
 
-f2 = man(f2, sc,se,gr,ye)
-
-f1.set_colorkey(wh)
-f2.set_colorkey(wh)
-
-f1 = pg.transform.scale(f1,(int(927/2)+250,768))
-f2 = pg.transform.scale(f2,(int(927/2)+250,768))
-
-screen.blit(f1,(0,0))
-screen.blit(f2,(int(927/2-100+250),0))
-
-pg.draw.rect(screen,lg,(0,0,927+500-100,90),0)
-pg.draw.rect(screen,bl,(0,0,927+500-100,90),1)
-screen.blit(textsurface,(0,0))
+#КОНЕЦ РИСУНКА
 
 pg.display.update() 
 clock = pg.time.Clock() 
@@ -121,4 +144,4 @@ while not finished:
         if event.type == pg.QUIT: 
             finished = True 
  
-pygame.quit()
+pg.quit()
